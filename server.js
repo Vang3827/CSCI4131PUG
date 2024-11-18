719c67c63f0c5153925e98eaba7c6098251edbe7
unknown <vang3827@umn.edu>
2024-11-18 09:44:52 -0600

    working on render gallery and query params

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use("/css", express.static("css/"))
-app.set("views", "resources");
+app.set("views", "templates");
 app.set("view engine", "pug");
 
 const port = 4131
@@ -11,6 +11,29 @@ app.get("/", (req, res)=>{
     res.render("main.pug")
 })
 
+app.get("/main", (req, res)=>{
+  res.render("main.pug")
+})
+
+app.get("/gallery", (req, res)=>{
+  console.log(req.query)
+  const queryTerm = req.query.query
+  const category = req.query.category
+
+  // res.send(`Search Query: ${queryTerm}, Category: ${category}`);
+  res.render('gallery', { renderQuery: queryTerm, renderCategory:category});
+})
+
+app.get("/create", (req, res)=>{
+  res.render("create.pug",(req,res))
+})
+
+app.post("/create", (req, res)=>{
+  res.render("create.pug",(req,res))
+})
+
+//app.get("listing/[id]") returns a html rendered verion of templates/listing.pug responds to a query and category
+//res.render ??
 
 app.listen(port , () => {
   console.log(`Example app listening on port ${port}`)
